e85642d369bd9a101995c6df185e24e990fceb48
Wang Kaimin <wangkaiminchn@gmail.com>
2016-07-27 16:39:45 +0800

    Initial Commit

---
+++ HypnoNerd/KAMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KAMAppDelegate.h
+//  HypnoNerd
+//
+//  Created by ka1em on 16-7-27.
+//  Copyright (c) 2016年 Ka1emApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by ka1em on 16-7-27.
+//  Copyright (c) 2016年 Ka1emApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KAMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KAMAppDelegate class]));
+    }
+}
