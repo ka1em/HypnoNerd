fb08364c818b29933d328c157f178a7eb69eef7c
Wang Kaimin <wangkaiminchn@gmail.com>
2016-07-28 11:09:11 +0800

    加载Nib文件

--- HypnoNerd/KAMAppDelegate.m
+++ HypnoNerd/KAMAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "KAMAppDelegate.h"
 #import "KAMHypnosisViewController.h"
+#import "KAMReminderViewController.h"
 
 @implementation KAMAppDelegate
 
@@ -18,7 +19,15 @@
     
     KAMHypnosisViewController *hvc = [[KAMHypnosisViewController alloc]init];
     
-    self.window.rootViewController = hvc;
+//    self.window.rootViewController = hvc;
+    
+    //获取指向NSBundle对象的指针，该NSBundle对象代表应用的主程序包
+    NSBundle *appBundle = [NSBundle mainBundle];
+    
+    //告诉初始化方法在appBundle中查找KAMReminderViewController.xib
+    KAMReminderViewController *rvc = [[KAMReminderViewController alloc]initWithNibName:@"KAMReminderViewController" bundle:appBundle];
+    
+    self.window.rootViewController = rvc;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
